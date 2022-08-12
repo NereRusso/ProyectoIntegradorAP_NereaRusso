b6f8dd48fa36093217c49cf2707ac1d478ee9196
NereRusso <russonere01@gmail.com>
2022-08-12 19:49:32 -0300

    últimos arreglos

--- PI_NereaRusso/src/app/app.module.ts
+++ PI_NereaRusso/src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -18,9 +16,7 @@ import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     LogoAPComponent,
-    RedesSocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
