c6b58cfe0b11a8da6bc3d65e811a6c51c76faabe
NereRusso <russonere01@gmail.com>
2022-08-12 18:06:56 -0300

    Proyecto

--- PI_NereaRusso/src/app/app.module.ts
+++ PI_NereaRusso/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysSkillsComponent } from './components/hys-skills/hys-skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HysSkillsComponent } from './components/hys-skills/hys-skills.component
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysSkillsComponent
+    HysSkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
